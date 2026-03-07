ba20c0ab5619ec241fbe66295ae6497a577e9734
Josef Hardi <josef.hardi@gmail.com>
2026-03-06 18:09:45 -0800

    feat: Add assay to CEDAR template IRI

--- evaluation/assays.py
+++ evaluation/assays.py
@@ -1,3 +1,18 @@
+ASSAY_SCHEMAS = {
+    "af": "https://repo.metadatacenter.org/templates/c9c6a02b-010e-4217-96dc-f7ef71dd14c4",
+    "atacseq": "https://repo.metadatacenter.org/templates/dd5e8653-81cf-470b-b71b-15cab421bb84",
+    "celldive": "https://repo.metadatacenter.org/templates/6f9eee7b-7ef1-4f32-a34e-706bbbbb09bf",
+    "codex": "https://repo.metadatacenter.org/templates/47c6071a-2ec7-46c1-94d9-6b5e2d7ac982",
+    "desi": "https://repo.metadatacenter.org/templates/01d909d8-84a8-4362-9e42-782bc4da0eec",
+    "histology": "https://repo.metadatacenter.org/templates/907d89c7-6cf4-4ec6-9edd-63cf0441d689",
+    "imc-2d": "https://repo.metadatacenter.org/templates/ce5524be-dab6-4668-97c1-8a5a09325e5f",
+    "lcms": "https://repo.metadatacenter.org/templates/ef090376-4e19-43cb-92c1-91a1d758ee6e",
+    "lightsheet": "https://repo.metadatacenter.org/templates/a4ff738c-a7e9-40c1-966e-22cf9c885fad",
+    "maldi": "https://repo.metadatacenter.org/templates/2e35434f-e6ed-4e01-a54a-189ec0706a3d",
+    "mibi": "https://repo.metadatacenter.org/templates/784cfaa7-4a73-4173-b639-b24e0ed76155",
+    "rnaseq": "https://repo.metadatacenter.org/templates/944e5fa0-f68b-4bdd-8664-74a3909429a9",
+}
+
 ASSAY_ORDER = [
     ("atacseq", "ATACseq"),
     ("rnaseq", "RNAseq"),
